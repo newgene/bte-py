ed4e214b8b62e3bef04690d342b5ea31a9d7e08d
sengineer0 <sengineer020@gmail.com>
2021-11-28 01:03:44 +0200

    tests: added new tests

--- biothings_explorer/query_graph_handler/__test__/integration/query_result_test.py
+++ biothings_explorer/query_graph_handler/__test__/integration/query_result_test.py
@@ -1,3 +1,4 @@
+import copy
 import unittest
 from biothings_explorer.query_graph_handler.query_node import QNode
 from biothings_explorer.query_graph_handler.query_edge import QEdge
@@ -52,8 +53,7 @@ class TestQueryResults(unittest.TestCase):
         }
         query_result = QueryResult()
         query_result.update({
-            'e01',
-            {
+            'e01': {
                 'connected_to': [],
                 'records': [record]
             }
@@ -395,3 +395,431 @@ class TestQueryResults(unittest.TestCase):
 
         edge1 = QEdge('e01', {'subject': gene_node_start, 'object': disease_node})
         edge2 = QEdge('e02', {'subject': disease_node, 'object': gene_node_end})
+
+        record1 = {
+            '$edge_metadata': {
+                'trapi_qEdge_obj': edge1,
+                'predicate': 'biolink:gene_associated_with_condition',
+                'api_name': 'Automat Pharos',
+            },
+            'publications': ['PMID:123', 'PMID:1234'],
+            '$input': {
+                'original': 'SYMBOL:KCNMA1',
+                'obj': [
+                    {
+                        'primaryID': 'NCBIGene:3778',
+                        'label': 'KCNMA1',
+                        'dbIDs': {
+                            'SYMBOL': 'KCNMA1',
+                            'NCBIGene': '3778',
+                        },
+                        'curies': ['SYMBOL:KCNMA1', 'NCBIGene:3778'],
+                    },
+                ],
+            },
+            '$output': {
+                'original': 'MONDO:0011122',
+                'obj': [
+                    {
+                        'primaryID': 'MONDO:0011122',
+                        'label': 'obesity disorder',
+                        'dbIDs': {
+                            'MONDO': '0011122',
+                            'MESH': 'D009765',
+                            'name': 'obesity disorder',
+                        },
+                        'curies': ['MONDO:0011122', 'MESH:D009765', 'name:obesity disorder'],
+                    },
+                ],
+            },
+        }
+
+        record2 = {
+            '$edge_metadata': {
+                'trapi_qEdge_obj': edge2,
+                'predicate': 'biolink:condition_associated_with_gene',
+                'api_name': 'Automat Hetio',
+            },
+            'publications': ['PMID:345', 'PMID:456'],
+            '$input': {
+                'original': 'SYMBOL:TULP3',
+                'obj': [
+                    {
+                        'primaryID': 'NCBIGene:7289',
+                        'label': 'TULP3',
+                        'dbIDs': {
+                            'SYMBOL': 'TULP3',
+                            'NCBIGene': '7289',
+                        },
+                        'curies': ['SYMBOL:TULP3', 'NCBIGene:7289'],
+                    },
+                ],
+            },
+            '$output': {
+                'original': 'MONDO:0011122',
+                'obj': [
+                    {
+                        'primaryID': 'MONDO:0011122',
+                        'label': 'obesity disorder',
+                        'dbIDs': {
+                            'MONDO': '0011122',
+                            'MESH': 'D009765',
+                            'name': 'obesity disorder',
+                        },
+                        'curies': ['MONDO:0011122', 'MESH:D009765', 'name:obesity disorder'],
+                    },
+                ],
+            },
+        }
+
+        query_result = QueryResult()
+        query_result.update({
+            'e01': {
+                'connected_to': ['e02'],
+                'records': [record1]
+            },
+            'e02': {
+                'connected_to': ['e02'],
+                'records': [record2]
+            }
+        })
+
+        results = query_result.get_results()
+        self.assertEqual(len(results), 1)
+        self.assertEqual(len(results[0]['node_bindings']), 3)
+        self.assertIn('n1', results[0]['node_bindings'])
+        self.assertIn('n2', results[0]['node_bindings'])
+        self.assertIn('n3', results[0]['node_bindings'])
+
+        self.assertEqual(len(results[0]['edge_bindings']), 2)
+        self.assertIn('e01', results[0]['edge_bindings'])
+        self.assertIn('e02', results[0]['edge_bindings'])
+
+        self.assertIn('score', results[0])
+
+    def test_query_graph_gene1_disease1_gene2_gene2_has_ids_param(self):
+        gene_node_start = QNode('n1', {'categories': ['Gene']})
+        disease_node = QNode('n2', {'categories': ['Disease']})
+        gene_node_end = QNode('n3', {'categories': ['Gene'], 'ids': ['NCBIGene:7289']})
+
+        edge1 = QEdge('e01', {'subject': gene_node_start, 'object': disease_node})
+        edge2 = QEdge('e02', {'subject': disease_node, 'object': gene_node_end})
+
+        record1 = {
+            '$edge_metadata': {
+                'trapi_qEdge_obj': edge1,
+                'predicate': 'biolink:gene_associated_with_condition',
+                'api_name': 'Automat Pharos',
+            },
+            'publications': ['PMID:123', 'PMID:1234'],
+            '$input': {
+                'original': 'SYMBOL:KCNMA1',
+                'obj': [
+                    {
+                        'primaryID': 'NCBIGene:3778',
+                        'label': 'KCNMA1',
+                        'dbIDs': {
+                            'SYMBOL': 'KCNMA1',
+                            'NCBIGene': '3778',
+                        },
+                        'curies': ['SYMBOL:KCNMA1', 'NCBIGene:3778'],
+                    },
+                ],
+            },
+            '$output': {
+                'original': 'MONDO:0011122',
+                'obj': [
+                    {
+                        'primaryID': 'MONDO:0011122',
+                        'label': 'obesity disorder',
+                        'dbIDs': {
+                            'MONDO': '0011122',
+                            'MESH': 'D009765',
+                            'name': 'obesity disorder',
+                        },
+                        'curies': ['MONDO:0011122', 'MESH:D009765', 'name:obesity disorder'],
+                    },
+                ],
+            },
+        }
+
+        record2 = {
+            '$edge_metadata': {
+                'trapi_qEdge_obj': edge2,
+                'predicate': 'biolink:condition_associated_with_gene',
+                'api_name': 'Automat Hetio',
+            },
+            'publications': ['PMID:345', 'PMID:456'],
+            '$input': {
+                'original': 'SYMBOL:TULP3',
+                'obj': [
+                    {
+                        'primaryID': 'NCBIGene:7289',
+                        'label': 'TULP3',
+                        'dbIDs': {
+                            'SYMBOL': 'TULP3',
+                            'NCBIGene': '7289',
+                        },
+                        'curies': ['SYMBOL:TULP3', 'NCBIGene:7289'],
+                    },
+                ],
+            },
+            '$output': {
+                'original': 'MONDO:0011122',
+                'obj': [
+                    {
+                        'primaryID': 'MONDO:0011122',
+                        'label': 'obesity disorder',
+                        'dbIDs': {
+                            'MONDO': '0011122',
+                            'MESH': 'D009765',
+                            'name': 'obesity disorder',
+                        },
+                        'curies': ['MONDO:0011122', 'MESH:D009765', 'name:obesity disorder'],
+                    },
+                ],
+            },
+        }
+
+        query_result = QueryResult()
+        query_result.update({
+            'e01': {
+                'connected_to': ['e02'],
+                'records': [record1]
+            },
+            'e02': {
+                'connected_to': ['e01'],
+                'records': [record2]
+            }
+        })
+
+        results = query_result.get_results()
+
+        self.assertEqual(len(results), 1)
+
+        self.assertEqual(len(results[0]['node_bindings']), 3)
+        self.assertIn('n1', results[0]['node_bindings'])
+        self.assertIn('n2', results[0]['node_bindings'])
+        self.assertIn('n3', results[0]['node_bindings'])
+
+        self.assertEqual(len(results[0]['edge_bindings']), 2)
+        self.assertIn('e01', results[0]['edge_bindings'])
+        self.assertIn('e02', results[0]['edge_bindings'])
+
+        self.assertIn('score', results[0])
+
+    def test_three_records_should_get_2_results_when_query_graph_is_dashdash_and_records_are_dashlessthan(self):
+        gene_node_start = QNode('n1', {'categories': ['Gene'], 'ids': ['NCBIGene:3778']})
+        disease_node = QNode('n2', {'categories': ['Disease']})
+        gene_node_end = QNode('n3', {'categories': ['Gene']})
+
+        edge1 = QEdge('e01', {'subject': gene_node_start, 'object': disease_node})
+        edge2 = QEdge('e02', {'subject': disease_node, 'object': gene_node_end})
+
+        record1 = {
+            '$edge_metadata': {
+                'trapi_qEdge_obj': edge1,
+                'predicate': 'biolink:gene_associated_with_condition',
+                'api_name': 'Automat Pharos',
+            },
+            'publications': ['PMID:123', 'PMID:1234'],
+            '$input': {
+                'original': 'SYMBOL:KCNMA1',
+                'obj': [
+                    {
+                        'primaryID': 'NCBIGene:3778',
+                        'label': 'KCNMA1',
+                        'dbIDs': {
+                            'SYMBOL': 'KCNMA1',
+                            'NCBIGene': '3778',
+                        },
+                        'curies': ['SYMBOL:KCNMA1', 'NCBIGene:3778'],
+                    },
+                ],
+            },
+            '$output': {
+                'original': 'MONDO:0011122',
+                'obj': [
+                    {
+                        'primaryID': 'MONDO:0011122',
+                        'label': 'obesity disorder',
+                        'dbIDs': {
+                            'MONDO': '0011122',
+                            'MESH': 'D009765',
+                            'name': 'obesity disorder',
+                        },
+                        'curies': ['MONDO:0011122', 'MESH:D009765', 'name:obesity disorder'],
+                    },
+                ],
+            },
+        }
+
+        record2 = {
+            '$edge_metadata': {
+                'trapi_qEdge_obj': edge2,
+                'predicate': 'biolink:condition_associated_with_gene',
+                'api_name': 'Automat Hetio',
+            },
+            'publications': ['PMID:345', 'PMID:456'],
+            '$input': {
+                'original': 'MONDO:0011122',
+                'obj': [
+                    {
+                        'primaryID': 'MONDO:0011122',
+                        'label': 'obesity disorder',
+                        'dbIDs': {
+                            'MONDO': '0011122',
+                            'MESH': 'D009765',
+                            'name': 'obesity disorder',
+                        },
+                        'curies': ['MONDO:0011122', 'MESH:D009765', 'name:obesity disorder'],
+                    },
+                ],
+            },
+            '$output': {
+                'original': 'SYMBOL:TULP3',
+                'obj': [
+                    {
+                        'primaryID': 'NCBIGene:7289',
+                        'label': 'TULP3',
+                        'dbIDs': {
+                            'SYMBOL': 'TULP3',
+                            'NCBIGene': '7289',
+                        },
+                        'curies': ['SYMBOL:TULP3', 'NCBIGene:7289'],
+                    },
+                ],
+            },
+        }
+
+        record3 = {
+            '$edge_metadata': {
+                'trapi_qEdge_obj': edge2,
+                'predicate': 'biolink:condition_associated_with_gene',
+                'api_name': 'Automat Hetio',
+            },
+            'publications': ['PMID:987', 'PMID:876'],
+            '$input': {
+                'original': 'MONDO:0011122',
+                'obj': [
+                    {
+                        'primaryID': 'MONDO:0011122',
+                        'label': 'obesity disorder',
+                        'dbIDs': {
+                            'MONDO': '0011122',
+                            'MESH': 'D009765',
+                            'name': 'obesity disorder',
+                        },
+                        'curies': ['MONDO:0011122', 'MESH:D009765', 'name:obesity disorder'],
+                    },
+                ],
+            },
+            '$output': {
+                'original': 'SYMBOL:TECR',
+                'obj': [
+                    {
+                        'primaryID': 'NCBIGene:9524',
+                        'label': 'TECR',
+                        'dbIDs': {
+                            'SYMBOL': 'TECR',
+                            'NCBIGene': '9524',
+                        },
+                        'curies': ['SYMBOL:TECR', 'NCBIGene:9524'],
+                    },
+                ],
+            },
+        }
+
+        query_result = QueryResult()
+
+        query_result.update({
+            'e01': {
+                'connected_to': ['e02'],
+                'records': [record1]
+            },
+            'e02': {
+                'connected_to': ['e01'],
+                'records': [record2, record3]
+            }
+        })
+
+        results = query_result.get_results()
+
+        self.assertEqual(len(results), 2)
+
+        self.assertEqual(len(results[0]['node_bindings']), 3)
+        self.assertIn('n1', results[0]['node_bindings'])
+        self.assertIn('n2', results[0]['node_bindings'])
+        self.assertIn('n3', results[0]['node_bindings'])
+
+        self.assertEqual(len(results[0]['edge_bindings']), 2)
+        self.assertIn('e01', results[0]['edge_bindings'])
+        self.assertIn('e02', results[0]['edge_bindings'])
+
+        self.assertIn('score', results[0])
+
+        self.assertEqual(len(results[1]['node_bindings']), 3)
+        self.assertIn('n1', results[1]['node_bindings'])
+        self.assertIn('n2', results[1]['node_bindings'])
+        self.assertIn('n3', results[1]['node_bindings'])
+
+        self.assertEqual(len(results[1]['edge_bindings']), 2)
+        self.assertIn('e01', results[1]['edge_bindings'])
+        self.assertIn('e02', results[1]['edge_bindings'])
+
+        self.assertIn('score', results[1])
+
+    def test_synthetic_records(self):
+        n0 = QNode('n0', {'categories': ['category_n0_n2'], 'ids': ['n0a']})
+        n1 = QNode('n1', {'categories': ['category_n1']})
+        n2 = QNode('n2', {'categories': ['category_n0_n2']})
+        n3 = QNode('n3', {'categories': ['biolink:category_n3']})
+        n4 = QNode('n4', {'categories': ['category_n4']})
+        n5 = QNode('n5', {'categories': ['category_n5']})
+
+        e0 = QEdge('e0', {'subject': n0, 'object': n1})
+
+        e1 = QEdge('e1', {'subject': n1, 'object': n2})
+        e2 = QEdge('e2', {'subject': n1, 'object': n3})
+        e3 = QEdge('e3', {'subject': n1, 'object': n4})
+
+        e4 = QEdge('e4', {'subject': n2, 'object': n5})
+        e5 = QEdge('e5', {'subject': n3, 'object': n5})
+        e6 = QEdge('e6', {'subject': n4, 'object': n5})
+
+        record0_n0a_n1a = {
+            '$edge_metadata': {
+                'trapi_qEdge_obj': e0,
+                'predicate': 'biolink:record0_pred0',
+                'source': 'source0',
+                'api_name': 'api0',
+            },
+            # n0
+            '$input': {
+                'obj': [
+                    {
+                        'primaryID': 'n0a',
+                    },
+                ],
+            },
+            # n1
+            '$output': {
+                'obj': [
+                    {
+                        'primaryID': 'n1a',
+                    },
+                ],
+            },
+        }
+
+        record0_n0a_n1a_pred0_api1 = copy.deepcopy(record0_n0a_n1a)
+        record0_n0a_n1a_pred0_api1['$edge_metadata']['source'] = 'source1'
+        record0_n0a_n1a_pred0_api1['$edge_metadata']['api_name'] = 'api1'
+
+        record0_n0a_n1a_pred1_api0 = copy.deepcopy(record0_n0a_n1a)
+        record0_n0a_n1a_pred1_api0['$edge_metadata']['predicate'] = 'biolink:record0_pred1'
+
+        record0_n0a_n1a_pred1_api1 = copy.deepcopy(record0_n0a_n1a)
+        record0_n0a_n1a_pred1_api1['$edge_metadata']['predicate'] = 'biolink:record0_pred1'
+        record0_n0a_n1a_pred1_api1['$edge_metadata']['source'] = 'source1'
+        record0_n0a_n1a_pred1_api1['$edge_metadata']['api_name'] = 'api1'
